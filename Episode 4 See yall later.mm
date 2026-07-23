0c4cd8af050deae0a597fe53347241447d5566e4
Joe Legner <joelegner@gmail.com>
2026-07-22 21:24:20 -0400

    Updates episode 4 mind map and adds a rendering of it.

--- Episode 4 See yall later.mm
+++ Episode 4 See yall later.mm
@@ -75,9 +75,99 @@
 </map_styles>
 </hook>
 <node TEXT="1. SET UP THE SEGMENTS." POSITION="bottom_or_right" ID="ID_486265118" CREATED="1784761985719" MODIFIED="1784762036614">
-<node TEXT="Set up the setup part." ID="ID_1210350957" CREATED="1784762054485" MODIFIED="1784762075303"/>
-<node TEXT="Do the setup part." ID="ID_775015618" CREATED="1784762075739" MODIFIED="1784762078717"/>
-<node TEXT="Clean up after the setup part." ID="ID_179965474" CREATED="1784762078956" MODIFIED="1784762085189"/>
+<node TEXT="Set up the setup part." ID="ID_1210350957" CREATED="1784762054485" MODIFIED="1784762075303">
+<node TEXT="Welcome to the show." ID="ID_659303620" CREATED="1784762450434" MODIFIED="1784762454129">
+<node TEXT="My name is Joe Legner." ID="ID_352096052" CREATED="1784767307193" MODIFIED="1784767313586"/>
+<node TEXT="This is my podcast." ID="ID_768263594" CREATED="1784767314434" MODIFIED="1784767420715"/>
+<node TEXT="You are listening to episode 4, season 1 of the see yall later podcast." ID="ID_543330052" CREATED="1784762703879" MODIFIED="1784762718519"/>
+<node TEXT="I will reveal the shocking reason why the show is called &quot;see yall later&quot; a few minutes." ID="ID_349749430" CREATED="1784767397659" MODIFIED="1784767521179"/>
+</node>
+<node TEXT="Orient the listener." ID="ID_691143966" CREATED="1784762454520" MODIFIED="1784762459722">
+<node TEXT="The title of episode 4 is &quot;How to Read a Book&quot;." ID="ID_1540376594" CREATED="1784762719126" MODIFIED="1784762730255"/>
+<node TEXT="Season 1 of the podcast is an experiment in evolutionary system improvement." ID="ID_788325998" CREATED="1784762730447" MODIFIED="1784762751375">
+<node TEXT="The governing principle is &quot;galls law&quot;, which states, &quot;any complex system that works evolved from a simpler system that worked.&quot;" ID="ID_1590691110" CREATED="1784762826524" MODIFIED="1784762858111"/>
+<node TEXT="The system in question is my &quot;podcast production system&quot;." ID="ID_1776688356" CREATED="1784762754047" MODIFIED="1784762766582"/>
+<node TEXT="I affectionately call it the &quot;PPS&quot; for short." ID="ID_540599228" CREATED="1784762766973" MODIFIED="1784762775055"/>
+<node TEXT="The function of the PPS is to produce the next podcast episode." ID="ID_1177035118" CREATED="1784762775605" MODIFIED="1784762791350"/>
+<node TEXT="Between episodes, we improve the system." ID="ID_129097558" CREATED="1784762794149" MODIFIED="1784762801990"/>
+<node TEXT="As long as we produce another episode, we have not broken the system." ID="ID_777122078" CREATED="1784762802133" MODIFIED="1784762812983"/>
+<node TEXT="The experiment is to see what happens given these rules." ID="ID_1273032845" CREATED="1784762813696" MODIFIED="1784762825685"/>
+</node>
+<node TEXT="The podcast itself is called &quot;see yall later&quot;, simply because that&apos;s what I naturally said when I ended episode 1." ID="ID_845824222" CREATED="1784762868221" MODIFIED="1784762894070"/>
+<node TEXT="I made that a policy." ID="ID_380572999" CREATED="1784762894252" MODIFIED="1784762898798">
+<node TEXT="As an aside, according to something called the Periodic Table of Arguments, there are three kinds of predicates in English: facts, values, and policies." ID="ID_757779174" CREATED="1784769113528" MODIFIED="1784769185225">
+<node TEXT="Predicates can be facts." ID="ID_1037606865" CREATED="1784769128615" MODIFIED="1784769133201">
+<node TEXT="For example, &quot;Rain fell in Beijing today&quot; is a fact." ID="ID_891820798" CREATED="1784769253155" MODIFIED="1784769510485"/>
+<node TEXT="The subject of this is &quot;rain&quot;." ID="ID_1057605654" CREATED="1784769278314" MODIFIED="1784769336883">
+<node TEXT="The subject is the thing we are talking about." ID="ID_1499384475" CREATED="1784769338873" MODIFIED="1784769344005"/>
+</node>
+<node TEXT="The predicate is &quot;fell in Beijing today&quot;." ID="ID_649603187" CREATED="1784769285787" MODIFIED="1784769298899">
+<node TEXT="The predicate is what we say about the thing we are talking about." ID="ID_1175080053" CREATED="1784769344857" MODIFIED="1784769352957"/>
+<node TEXT="A predicate like this says something that is independently verifiable." ID="ID_1260664224" CREATED="1784769457748" MODIFIED="1784769474926"/>
+</node>
+</node>
+<node TEXT="Predicates can be value judgements." ID="ID_1078478609" CREATED="1784769133391" MODIFIED="1784769138577">
+<node TEXT="For example, &quot;That flower is pretty&quot; has a predicate that expresses a value." ID="ID_1641818496" CREATED="1784769356940" MODIFIED="1784769704696"/>
+<node TEXT="The value is &quot;pretty&quot;." ID="ID_800841750" CREATED="1784769705679" MODIFIED="1784769712463"/>
+<node TEXT="The subject this time is &quot;That flower.&quot;" ID="ID_1927100472" CREATED="1784769377858" MODIFIED="1784769719535"/>
+<node TEXT="The predicate is &quot;is pretty&quot;." ID="ID_1279785809" CREATED="1784769391588" MODIFIED="1784769728969"/>
+<node TEXT="The clue word here is &quot;pretty&quot;." ID="ID_230753016" CREATED="1784769409764" MODIFIED="1784769732457">
+<node TEXT="&quot;Pretty&quot; is a value judgement." ID="ID_1239966286" CREATED="1784769425508" MODIFIED="1784769738937"/>
+<node TEXT="It is not independently verifiable." ID="ID_1181320023" CREATED="1784769430059" MODIFIED="1784769453373"/>
+</node>
+</node>
+<node TEXT="Predicates can be policies." ID="ID_1882736547" CREATED="1784769138753" MODIFIED="1784769147929">
+<node TEXT="For example, &quot;I should end each show by saying, &apos;See yall later&apos;&quot; is a policy." ID="ID_1150134237" CREATED="1784769483485" MODIFIED="1784769657912"/>
+<node TEXT="The subject is me." ID="ID_1313961740" CREATED="1784769515317" MODIFIED="1784769538558"/>
+<node TEXT="The predicate is &quot;should bring my umbrella&quot;." ID="ID_1910551659" CREATED="1784769538693" MODIFIED="1784769545422"/>
+<node TEXT="The clue word is &quot;should&quot;." ID="ID_1199727605" CREATED="1784769545622" MODIFIED="1784769550775"/>
+<node TEXT="When you see &quot;should&quot;, it is nearly certain that you are dealing with a policy predicate." ID="ID_1589729941" CREATED="1784769550917" MODIFIED="1784769568359"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Reminder listeners of the structure." ID="ID_1143386089" CREATED="1784762490384" MODIFIED="1784762496801">
+<node TEXT="Each episode is highly structured." ID="ID_187614988" CREATED="1784762549928" MODIFIED="1784762910091"/>
+<node TEXT="The main content is contained in three segments." ID="ID_437900104" CREATED="1784762944196" MODIFIED="1784762955464"/>
+<node TEXT="The topic is discussed in two out of 3 segments." ID="ID_464712426" CREATED="1784762922605" MODIFIED="1784762932789">
+<node TEXT="The first segment is a theoretical discussion of the episode topic." ID="ID_598352645" CREATED="1784762605984" MODIFIED="1784762965830"/>
+<node TEXT="The second segment is a practical discussion of the episode topic." ID="ID_1272208229" CREATED="1784762622192" MODIFIED="1784762970278"/>
+<node TEXT="The third segment is special." ID="ID_841043010" CREATED="1784762630759" MODIFIED="1784762634961"/>
+<node TEXT="It is called the &quot;musical segment&quot;." ID="ID_1982833617" CREATED="1784762635134" MODIFIED="1784762643120"/>
+</node>
+</node>
+</node>
+<node TEXT="Do the setup part." ID="ID_775015618" CREATED="1784762075739" MODIFIED="1784762078717">
+<node TEXT="Introduce the episode topic." ID="ID_315479331" CREATED="1784762498368" MODIFIED="1784762541336">
+<node TEXT="The episode topic for today is the 1972 edition of a book titled &quot;How to Read a Book&quot; by &quot;hyper-nerds&quot; Mortimer J. Adler and Charles Van Doren." POSITION="bottom_or_right" ID="ID_1175781867" CREATED="1784762591239" MODIFIED="1784763043334">
+<node TEXT="The first two subjects discuss the episode topic." ID="ID_1531215272" CREATED="1784766905926" MODIFIED="1784766921125">
+<node TEXT="The first segment is the theoretical discussion." ID="ID_1952153060" CREATED="1784766921788" MODIFIED="1784766929941"/>
+<node TEXT="The second segment is the practical discussion." ID="ID_314966953" CREATED="1784766930452" MODIFIED="1784766940221"/>
+</node>
+<node TEXT="How do we differentiate between theoretical and practical?" ID="ID_1598000131" CREATED="1784766941109" MODIFIED="1784766950877">
+<node TEXT="Both the theoretical and the practical segments try to do two things." ID="ID_500872679" CREATED="1784766951363" MODIFIED="1784766987412">
+<node TEXT="First, they ask questions." ID="ID_1497145321" CREATED="1784766989076" MODIFIED="1784766996916"/>
+<node TEXT="Second, they try to answer those questions." ID="ID_231938295" CREATED="1784766997084" MODIFIED="1784767002820"/>
+</node>
+<node TEXT="The difference is the kinds of questions." ID="ID_887150148" CREATED="1784767003364" MODIFIED="1784767012787">
+<node TEXT="The theoretical segment discusses questions that can be answered by &quot;knowing&quot; something." ID="ID_1454145147" CREATED="1784767013003" MODIFIED="1784767026620"/>
+<node TEXT="The practical segment discusses questions that can be answered by &quot;doing&quot; something." ID="ID_549715471" CREATED="1784767026971" MODIFIED="1784767040012"/>
+</node>
+<node TEXT="We will see this in action together shortly." ID="ID_1911064025" CREATED="1784767049915" MODIFIED="1784767063868"/>
+</node>
+</node>
+</node>
+<node TEXT="Introduce the music segment topic." ID="ID_278121801" CREATED="1784762523135" MODIFIED="1784762534761">
+<node TEXT="As always, the third segment is the &quot;musical segment&quot;." ID="ID_741240525" CREATED="1784766871621" MODIFIED="1784767071628"/>
+<node TEXT="Today we will talk about the Frank Sinatra song New York, New York." ID="ID_1709307749" CREATED="1784766887499" MODIFIED="1784766900140"/>
+<node TEXT="I am trying to learn it as part of my dueling pianos career." ID="ID_1607320930" CREATED="1784767075715" MODIFIED="1784767084084"/>
+<node TEXT="Later this year we have a cruise contract from New York." ID="ID_767308696" CREATED="1784767094372" MODIFIED="1784767101021"/>
+<node TEXT="It would be nice to have this one ready by the." ID="ID_1046442799" CREATED="1784767101172" MODIFIED="1784767106710"/>
+</node>
+</node>
+<node TEXT="Clean up after the setup part." ID="ID_179965474" CREATED="1784762078956" MODIFIED="1784762085189">
+<node TEXT="With that, I judge that we are ready to transition from part 1 to part 2." ID="ID_868470147" CREATED="1784767188883" MODIFIED="1784767216811"/>
+</node>
 </node>
 <node TEXT="2. DO THE SEGMENTS." POSITION="bottom_or_right" ID="ID_1926935798" CREATED="1784762016503" MODIFIED="1784762045982">
 <node TEXT="Set up the segments part." ID="ID_194006347" CREATED="1784762086501" MODIFIED="1784762090606">
@@ -89,18 +179,33 @@
 <node TEXT="And the segments part is also where we do the fun &quot;musical segment&quot;." ID="ID_1031436184" CREATED="1784762191517" MODIFIED="1784762241725"/>
 </node>
 <node TEXT="Do the segments part." ID="ID_687859958" CREATED="1784762090749" MODIFIED="1784762094421">
-<node TEXT="Do the theory segment." ID="ID_1435758666" CREATED="1784762251635" MODIFIED="1784762255036"/>
-<node TEXT="Do the practice segment." ID="ID_430652811" CREATED="1784762255195" MODIFIED="1784762258836"/>
-<node TEXT="Do the musical segment." ID="ID_1490416399" CREATED="1784762259019" MODIFIED="1784762265044"/>
+<node TEXT="Do the theory segment." ID="ID_1435758666" CREATED="1784762251635" MODIFIED="1784769081122">
+<icon BUILTIN="full-1"/>
 </node>
-<node TEXT="Clean up after the segments part." ID="ID_1853201249" CREATED="1784762094660" MODIFIED="1784762099465"/>
+<node TEXT="Do the practice segment." ID="ID_430652811" CREATED="1784762255195" MODIFIED="1784769090715">
+<icon BUILTIN="full-2"/>
+</node>
+<node TEXT="Do the musical segment." ID="ID_1490416399" CREATED="1784762259019" MODIFIED="1784769096627">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node TEXT="Clean up after the segments part." ID="ID_1853201249" CREATED="1784762094660" MODIFIED="1784762099465">
+<node TEXT="We have talked about the book How to Read a Book." ID="ID_32648954" CREATED="1784767227772" MODIFIED="1784767236467"/>
+<node TEXT="We have played around with New York, New York by Frank Sinatra." ID="ID_1325011567" CREATED="1784767236602" MODIFIED="1784767249483"/>
+<node TEXT="That&apos;s all the content we have for today." ID="ID_1376913411" CREATED="1784767250178" MODIFIED="1784767259218"/>
+</node>
 </node>
 <node TEXT="3. CLEAN UP AFTER THE SEGMENTS." POSITION="bottom_or_right" ID="ID_879877836" CREATED="1784761999317" MODIFIED="1784762049740">
-<node TEXT="Set up the clean up." ID="ID_426106839" CREATED="1784762100989" MODIFIED="1784762108445"/>
-<node TEXT="Do the cleanup." ID="ID_1852145176" CREATED="1784762108644" MODIFIED="1784762111906"/>
+<node TEXT="Set up the clean up." ID="ID_426106839" CREATED="1784762100989" MODIFIED="1784762108445">
+<node TEXT="That means it is time to close out the show." ID="ID_805032803" CREATED="1784767262516" MODIFIED="1784767268994"/>
+</node>
+<node TEXT="Do the cleanup." ID="ID_1852145176" CREATED="1784762108644" MODIFIED="1784762111906">
+<node TEXT="This has been episode 4, season 1 of see yall later podcast." ID="ID_1869441873" CREATED="1784762140130" MODIFIED="1784762151643"/>
+<node TEXT="You can find this episode and more online at the link provided." ID="ID_138367427" CREATED="1784767137978" MODIFIED="1784767151379"/>
+<node TEXT="Or you can search for &quot;See yall later&quot; on Apple Podcasts." ID="ID_1917983854" CREATED="1784767151986" MODIFIED="1784767164852"/>
+</node>
 <node TEXT="Clean up after the cleanup." ID="ID_309401877" CREATED="1784762112121" MODIFIED="1784762120962">
 <node TEXT="I&apos;m Joe Legner." ID="ID_1141020859" CREATED="1784762121433" MODIFIED="1784762125510"/>
-<node TEXT="This has been episode 4, season 1 of see yall later podcast." ID="ID_1869441873" CREATED="1784762140130" MODIFIED="1784762151643"/>
 <node TEXT="See yall later." ID="ID_1043384347" CREATED="1784762125645" MODIFIED="1784762138689"/>
 </node>
 </node>
